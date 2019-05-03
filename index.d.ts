d24e6c6c7c0bd2c6ebbc0647d07cd46042cd0849
ech_beku <b.kuster@esri.ch>
2019-05-03 13:50:10 +0200

    a bit of documentation inside the types

--- index.d.ts
+++ index.d.ts
@@ -33,9 +33,39 @@ declare module 'arcgoose' {
 
 
     interface Model<T> {
+        /**
+         * Use to add, update or delete features
+         */
         applyEdits(): ApplyEdits<T>
-        deleteWhere(info: Partial<T>): Promise<Deleted>;
+
+        /**
+         * deletes all features which match the whereClause
+         * @param whereClause sql-like where clause
+         */
+        deleteWhere(whereClause: string): Promise<Deleted>;
+
+        /**
+         * finds a set of features
+         * @param info object-based definition to filter features
+         * @example ```
+         *   const features = await model.find({
+         *       city: 'Zürich'
+         *   }).exec()
+         * ```
+         */
         find(info?: Partial<T>): Find<T>;
+
+        /**
+         * finds a set of features
+         * 
+         * will throw an exception if the resultset does not contain exactly one feature
+         * @param info object-based definition to filter features
+         * @example ```typescript
+         * const features = await model.findOne({
+             *     objectid: 1
+         * }).exec()
+         * ```
+         * */
         findOne(info?: Partial<T>): Find<T>;
     }
 
@@ -45,28 +75,104 @@ declare module 'arcgoose' {
     }
 
     interface Find<T> {
+        /**
+         * uses `esriSpatialRelContains` to query by geometry
+         */
         contains(): Find<T>
+
+        /**
+         * executes the query, will resolve when all features are fetched
+         */
         exec(): Promise<Array<Feature<T>>>
+
+        /**
+         * adds a where clause, if multiple added, they will be joined with
+         * `and`
+         */
         filter(sqlWhere: string): Find<T>
-        geometry(geometry, geometryType): Find<T>
+
+        /**
+         * sets the geometry for a spatial query
+         * @param geometry ArcGIS Rest Geometry Representation as Object
+         * @param geometryType type of the input geometry
+         */
+        geometry(geometry, geometryType: 'esriGeometryPoint' | 'esriGeometryMultipoint' | 'esriGeometryPolyline' | 'esriGeometryPolygon' | 'esriGeometryEnvelope'): Find<T>
+        
+        /**
+         * uses `esriSpatialRelIntersects` to query by geometry
+         */
         intersects(): Find<T>
+
+        /**
+         * limits the maximal amount of features
+         * @param amount maximal count of features to retrieve
+         */
         limit(amount: number): Find<T>
+
+        /**
+         * sets an offset to use with `.limit`
+         * @param amount offset at which feature-count to start
+         */
         offset(amount: number): Find<T>
+
+        /**
+         * 
+         * @param wkid spatial reference of the output features
+         */
         outSpatialReference(wkid: number): Find<T>
+
+        /**
+         * a definition to aggregate data
+         * @param outStatistics some outstatistic definitions
+         * @param groupByFieldsForStatistics fields which should be grouped by
+         */
         outStatistics(
             outStatistics: OutStatistics<T>[],
             groupByFieldsForStatistics: Array<keyof T>
         ): FindOutStatistic<T> 
 
+        /**
+         * if specified, only these fields will be fetched from the service
+         * @param outFields fields to populate in out features
+         */
         populate(outFields: Array<keyof T>): Find<T>
+
+        /**
+         * if set, out features will contain centroids
+         */
         returnCentroid(): Find<T>
+
+        /**
+         * if set, out features will contain geometries
+         */
         returnGeometry(): Find<T>
+
+        /**
+         * if set, out features geometries will contain z-values
+         */
         returnZ(): Find<T>
 
+        /**
+         * 
+         * @param sortOrder object with key-map, use positive value for ascending, negative for descending
+         * @example ```typescript
+         * // returns features, sorted descending by objectid and ascending by name
+         * const features = await model.sort({
+         *      objectId: -1,
+         *      name: 1
+         * }).exec()
+         * ```
+         */
         sort(sortOrder: {
             [P in keyof T]?: number
         }): Find<T>
 
+        /**
+         * same as filter, adds a where clause part.
+         * 
+         * all parts are joined with `and`
+         * @param sqlFilter where clause
+         */
         where(sqlFilter): Find<T>
     }
 
@@ -87,11 +193,20 @@ declare module 'arcgoose' {
     }
 
     interface ApplyEdits<T> {
+
         add(features: Feature<T> | Array<Feature<T>>): ApplyEdits<T>
         update(features: Feature<T> | Array<Feature<T>>): ApplyEdits<T>
         delete(featureIds: any | Array<any>): ApplyEdits<T>
 
+        /**
+         * uses global ids for deletion
+         * @default true
+         */
         useGlobalIds(): ApplyEdits<T>
+
+        /**
+         * uses object ids for deletion
+         */
         useObjectIds(): ApplyEdits<T>
 
         handle(): Handle
@@ -117,10 +232,41 @@ declare module 'arcgoose' {
         }
     }
 
+    /**
+     * establishes a connection to a feature service
+     * @param config url or portal item
+     * @example ```typescript
+     * // `Model.ServiceDefinition` can be generated with `npx arcgoose-typings`
+     * const connection = await arcgoose.connect<Model.ServiceDefinition>({
+     *      url: 'https://url-to-the-feature-service/arcgis/rest/services/Something/FeatureServer'
+     * })
+     * ```
+     */
     export function connect<T>(config: { url: string }): Connection & T;
 
+    /**
+     * creates an endpoint for a specific layer / table
+     * 
+     * @param info information about a specific layer of the feature service
+     * @param schema schema of the layer passed in info
+     * 
+     * @example ```typescript
+     * // Model.FeatureClass, connetion.layers.FeatureClass and Model.FeatureClassSchema
+     * // are autogenerated by `npx arcgoose-typings`
+     * const model = await arcgoose.model<Model.FeatureClass>(
+     *      connection.layers.FeatureClass, 
+     *      Model.FeatureClassSchema
+     * )
+     * ```
+     */
     export function model<T>(info: Info, schema: Schema): Promise<Model<T>>;
 
+    /**
+     * uploads a bunch of handles
+     * 
+     * @param handles a list with handles, collected from applyEdits().handle()
+     * @param progressCallback callback which reports the state of progress
+     */
     export function execAll(handles: Handle[], progressCallback: (percentage: number) => void): Promise<Array<ApplyEditsResult>>
 
     export function typings(connection: Connection): string
