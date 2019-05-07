5d8bdf5e2237040c4cb4c3d14293de5333a75f41
ech_beku <b.kuster@esri.ch>
2019-05-07 15:32:19 +0200

    fixed lint-related issues in cli and typings

--- src/typings/cli.js
+++ src/typings/cli.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+/* eslint-disable no-console */
 
 // Copyright 2017-2019 by Esri
 // Beda Kuster
@@ -15,26 +16,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const typings = require('./index')
+const typings = require('./index');
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 
 if (process.argv.length < 4) {
-    console.log('')
-    console.log('')
-    console.log('******************************************************************')
-    console.log('This tool will generate TypeScript Typings for your FeatureService')
-    console.log('It will give you Type-Completion while you are using arcgoose')
-    console.log('******************************************************************')
-    console.log('Usage:')
-    console.log('   - npx arcgoose-typings <FeatureServiceURL> <OutputFileName>')
-    console.log('Options')
-    console.log('   --include-docs: Adds Docs to your model with infos from FeatureService JSON')
-    console.log('   --use-alias: takes field alias instead of field names')
-    console.log('')
-    console.log('If your service is secured, use FeatureServiceURL with appended Token ?token=...')
-    console.log('')
-    process.exit()
+  console.log('');
+  console.log('');
+  console.log('******************************************************************');
+  console.log('This tool will generate TypeScript Typings for your FeatureService');
+  console.log('It will give you Type-Completion while you are using arcgoose');
+  console.log('******************************************************************');
+  console.log('Usage:');
+  console.log('   - npx arcgoose-typings <FeatureServiceURL> <OutputFileName>');
+  console.log('Options');
+  console.log('   --include-docs: Adds Docs to your model with infos from FeatureService JSON');
+  console.log('   --use-alias: takes field alias instead of field names');
+  console.log('');
+  console.log('If your service is secured, use FeatureServiceURL with appended Token ?token=...');
+  console.log('');
+  process.exit();
 }
 
 
@@ -42,53 +43,45 @@ let featureServiceUrl = process.argv[2];
 const output = process.argv[3];
 
 if (featureServiceUrl.indexOf('?') < 0) {
-    featureServiceUrl += '?f=json'
+  featureServiceUrl += '?f=json';
 } else {
-    featureServiceUrl += `&f=json`
+  featureServiceUrl += '&f=json';
 }
 
 function retrieveItemDetails(id) {
-    return new Promise(resolve => {
-        let toks = featureServiceUrl.split("?")
-        let requestUrl = `${toks[0]}/${id}?${toks[1]}` 
-        request(requestUrl, (err, response, body) => {
-            resolve(JSON.parse(body))
-        })
-    })
+  return new Promise((resolve) => {
+    const toks = featureServiceUrl.split('?');
+    const requestUrl = `${toks[0]}/${id}?${toks[1]}`;
+    request(requestUrl, (err, response, body) => {
+      resolve(JSON.parse(body));
+    });
+  });
 }
 
 request(featureServiceUrl, (err, response, body) => {
-    const j = JSON.parse(body)
-    const connection = {
-        ...j,
-        layers: {},
-        tables: {},
-    }
+  const j = JSON.parse(body);
+  const connection = {
+    ...j,
+    layers: {},
+    tables: {},
+  };
 
-    let proms = j.layers.map(layer => retrieveItemDetails(layer.id)
-                                .then(result => {
-                                    connection.layers[layer.name] = result
-                                }))
-
-                                
-    proms.push(...j.tables.map(layer => retrieveItemDetails(layer.id)
-                                .then(result => {
-                                    connection.tables[layer.name] = result
-                                })))
-
-    Promise.all(proms).then(stuff => {
-        const types = typings(connection, {
-            includeDocs: process.argv.filter(arg => arg === '--include-docs').length === 1,
-            useAlias: process.argv.filter(arg => arg === '--use-alias').length === 1})
-        fs.writeFileSync(output, types);
-    })
-})
+  const proms = j.layers.map(layer => retrieveItemDetails(layer.id)
+    .then((result) => {
+      connection.layers[layer.name] = result;
+    }));
 
 
-// arcgoose.default.connect({
-//     url: featureServiceUrl
-// }).then(connection => {
-//     const types = arcgoose.default.typings(connection);
+  proms.push(...j.tables.map(layer => retrieveItemDetails(layer.id)
+    .then((result) => {
+      connection.tables[layer.name] = result;
+    })));
 
-//     fs.writeFileSync(output, types);
-// })
+  Promise.all(proms).then(() => {
+    const types = typings(connection, {
+      includeDocs: process.argv.filter(arg => arg === '--include-docs').length === 1,
+      useAlias: process.argv.filter(arg => arg === '--use-alias').length === 1,
+    });
+    fs.writeFileSync(output, types);
+  });
+});
--- src/typings/index.js
+++ src/typings/index.js
@@ -16,93 +16,88 @@
 
 // grabbed from here: https://developers.arcgis.com/rest/services-reference/layer-feature-service-.htm
 const arcgisTsFieldMap = {
-    'esriFieldTypeOID': 'number',
-    'esriFieldTypeInteger': 'number',
-    'esriFieldTypeSmallInteger': 'number',
-    'esriFieldTypeSingle': 'number',
-    'esriFieldTypeDouble': 'number',
-    'esriFieldTypeDate': 'Date',
-    'esriFieldTypeString': 'string',
-    'esriFieldTypeGUID': 'string'
-}
+  esriFieldTypeOID: 'number',
+  esriFieldTypeInteger: 'number',
+  esriFieldTypeSmallInteger: 'number',
+  esriFieldTypeSingle: 'number',
+  esriFieldTypeDouble: 'number',
+  esriFieldTypeDate: 'Date',
+  esriFieldTypeString: 'string',
+  esriFieldTypeGUID: 'string',
+};
 
 // grabbed from here: https://developers.arcgis.com/rest/services-reference/layer-feature-service-.htm
 const arcgisTsSchemaFieldMap = {
-    'esriFieldTypeOID': 'Number',
-    'esriFieldTypeInteger': 'Number',
-    'esriFieldTypeSmallInteger': 'Number',
-    'esriFieldTypeSingle': 'Number',
-    'esriFieldTypeDouble': 'Number',
-    'esriFieldTypeDate': 'Date',
-    'esriFieldTypeString': 'String',
-    'esriFieldTypeGUID': 'String'
-}
+  esriFieldTypeOID: 'Number',
+  esriFieldTypeInteger: 'Number',
+  esriFieldTypeSmallInteger: 'Number',
+  esriFieldTypeSingle: 'Number',
+  esriFieldTypeDouble: 'Number',
+  esriFieldTypeDate: 'Date',
+  esriFieldTypeString: 'String',
+  esriFieldTypeGUID: 'String',
+};
 
-const sanatizeName = str => str.replace(/[\(\) \[\]\{\}]/ig, '')
+// eslint-disable-next-line no-useless-escape
+const sanatizeName = str => str.replace(/[\(\) \[\]\{\}]/ig, '');
 
 const camelCase = str => str.charAt(0).toLowerCase() + str.slice(1);
 
+// eslint-disable-next-line prefer-template
+const markdownForValue = value => '\n```\n' + JSON.stringify(value, null, 4) + '\n```\n';
+
 const jdocForField = (includeDocs, field, title) => {
-    if(!includeDocs) return ''
-    return `
+  if (!includeDocs) return '';
+  return `
     /**
      * ${title || 'ArcGIS Service Definition'}:
      *
      * ${Object.keys(field).slice(0, 10).map(p => `${p}: ${markdownForValue(field[p])}`).join(' * ')}
      * 
      * */
-    `
-}
+    `;
+};
 
-const markdownForValue = value => {
-    return '\n```\n' + JSON.stringify(value, null, 4) + '\n```\n'
-}
 
-const fieldToInterfaceContent = (field, {includeDocs, useAlias}) => {
-    return `${jdocForField(includeDocs, field)}
-            '${useAlias ? field.alias : field.name}': ${arcgisTsFieldMap[field.type]}`
-}
+const fieldToInterfaceContent = (field, { includeDocs, useAlias }) => `${jdocForField(includeDocs, field)}
+            '${useAlias ? field.alias : field.name}': ${arcgisTsFieldMap[field.type]}`;
 
-const fieldToSchemaContent = (field, {includeDocs, useAlias}) => {
-    return `${jdocForField(includeDocs, field)}
+const fieldToSchemaContent = (field, { includeDocs, useAlias }) => `${jdocForField(includeDocs, field)}
         '${field.name}': {
         type: ${arcgisTsSchemaFieldMap[field.type]}
         ${useAlias ? `,alias: '${field.alias}` : ''}
-    } `
-}
+    } `;
 
-const definition = (item, {includeDocs, useAlias}) => {
-    const sanName = sanatizeName(item.name)
-    return `
+const definition = (item, { includeDocs, useAlias }) => {
+  const sanName = sanatizeName(item.name);
+  return `
     ${jdocForField(includeDocs, item, 'Feature-Definition')}
     export interface ${sanName}Feature extends arcgoose.Feature<${sanName}> {}
     
     ${jdocForField(includeDocs, item, 'Attribute-Definition')}
     export interface ${sanName} {
-        ${ item.fields.map(field => fieldToInterfaceContent(field, {includeDocs, useAlias})).join('\n')}
+        ${item.fields.map(field => fieldToInterfaceContent(field, { includeDocs, useAlias })).join('\n')}
     }
 
     ${jdocForField(includeDocs, item, 'Schema Description')}
     export const ${camelCase(sanName)}Schema: arcgoose.Schema = {
-        ${item.fields.map(field => fieldToSchemaContent(field, {includeDocs, useAlias})).join(',\n')}
+        ${item.fields.map(field => fieldToSchemaContent(field, { includeDocs, useAlias })).join(',\n')}
     }    
-    `
-}
+    `;
+};
 
-module.exports = ({ layers, tables }, {includeDocs, useAlias}) => {
+module.exports = ({ layers, tables }, { includeDocs, useAlias }) => {
+  const lrs = Object.keys(layers).map(layer => layers[layer]);
+  const tbls = Object.keys(tables).map(table => tables[table]);
 
-    
-    const lrs = Object.keys(layers).map(layer => layers[layer])
-    const tbls = Object.keys(tables).map(table => tables[table])
+  const allItems = lrs.concat(tbls);
 
-    let allItems = lrs.concat(tbls)
-
-    let output = `
+  const output = `
     
     // tslint:disable
     import arcgoose from 'arcgoose';
 
-    ${allItems.map(item => definition(item, {includeDocs, useAlias})).join('')}
+    ${allItems.map(item => definition(item, { includeDocs, useAlias })).join('')}
     
     export interface ServiceDefinition {
         layers: {
@@ -113,6 +108,6 @@ module.exports = ({ layers, tables }, {includeDocs, useAlias}) => {
         }
     }
     
-    `
-    return output
-}
+    `;
+  return output;
+};
